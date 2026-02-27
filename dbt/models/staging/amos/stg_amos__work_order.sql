ded507e629a75801863fe07504b513f25b85ed30
Ashish-Vajrapu-2001 <Vajrapu.Ashish.20022001@gmail.com>
2026-02-27 20:19:58 +0530

    Add silver layer CDC pipeline code

---
+++ dbt/models/staging/amos/stg_amos__work_order.sql
@@ -0,0 +1,64 @@
+{{ config(
+    materialized='incremental',
+    incremental_strategy='merge',
+    unique_key='wo_number',
+    merge_exclude_columns=['_silver_load_timestamp'],
+    on_schema_change='append_new_columns',
+    tags=['silver', 'amos']
+) }}
+
+WITH source AS (
+    SELECT
+        WO_NUMBER,
+        AIRCRAFT_REG,
+        TOTAL_COST,
+        _FIVETRAN_SYNCED,
+        _FIVETRAN_DELETED
+    FROM {{ source('bronze_amos', 'WORK_ORDER') }}
+
+    {% if is_incremental() %}
+        WHERE _FIVETRAN_SYNCED > (
+            SELECT COALESCE(
+                MAX(_bronze_sync_timestamp),
+                '{{ var("min_date", "2000-01-01") }}'::TIMESTAMP_TZ
+            )
+            FROM {{ this }}
+        )
+        OR _FIVETRAN_SYNCED >= DATEADD(
+            'day',
+            -{{ var('silver_lookback_days', 3) }},
+            CURRENT_TIMESTAMP()
+        )
+    {% endif %}
+),
+
+active_records AS (
+    SELECT *
+    FROM source
+    WHERE _FIVETRAN_DELETED = FALSE
+       OR _FIVETRAN_DELETED IS NULL
+),
+
+deduped AS (
+    SELECT *,
+        ROW_NUMBER() OVER (
+            PARTITION BY WO_NUMBER
+            ORDER BY _FIVETRAN_SYNCED DESC
+        ) AS _rn
+    FROM active_records
+),
+
+transformed AS (
+    SELECT
+        WO_NUMBER,
+        AIRCRAFT_REG,
+        TOTAL_COST,
+        _FIVETRAN_SYNCED          AS _bronze_sync_timestamp,
+        CURRENT_TIMESTAMP()       AS _silver_load_timestamp,
+        '{{ invocation_id }}'     AS _dbt_run_id,
+        _FIVETRAN_DELETED         AS _is_deleted
+    FROM deduped
+    WHERE _rn = 1
+)
+
+SELECT * FROM transformed
